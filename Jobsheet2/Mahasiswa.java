f6241b2618dfdfcbc31432346955fda80098a073
rafaeldimaass <rafadimas29@gmail.com>
2026-02-20 10:23:18 +0700

    Percobaan 1 class

---
+++ Jobsheet2/Mahasiswa.java
@@ -0,0 +1,34 @@
+package Jobsheet2;
+
+public class Mahasiswa {
+    String nim, nama, kelas;
+    double ipk;
+
+    void tampilkanInformasi() {
+        System.out.println("Nama: " + nama);
+        System.out.println("NIM: " + nim);
+        System.out.println("Kelas: " + kelas);
+        System.out.println("IPK: " + ipk);
+    }
+
+    void ubahKelas(String kelasBaru) {
+        kelas = kelasBaru;
+    }
+
+    void updateIpk(double ipkBaru) {
+        
+        ipk = ipkBaru;
+    }
+
+    String nilaiKinerja(double ipk) {
+        if (ipk >= 3.5) {
+            return "Kinerja sangat baik";
+        } else if (ipk >= 3.0) {
+            return "Kinerja baik";
+        } else if (ipk >= 2.0) {
+            return "Kinerja cukup";
+        } else {
+            return "Kinerja kurang";
+        }
+    }
+}
