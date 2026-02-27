be45f66c7b9c9c68c7d7276711b86a996215b40f
rafaeldimaass <rafadimas29@gmail.com>
2026-02-27 17:59:02 +0700

    Percobaan 3

---
+++ Jobsheet3/MataKuliah.java
@@ -0,0 +1,13 @@
+package Jobsheet3;
+
+public class MataKuliah {
+    public String kode, nama;
+    public int sks, jumlahJam;
+
+    public MataKuliah(String kode, String nama, int sks, int jumlahJam) {
+        this.kode = kode;
+        this.nama = nama;
+        this.sks = sks;
+        this.jumlahJam = jumlahJam;
+    }
+}
