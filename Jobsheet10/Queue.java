d6012421373f9da66a082507f811026280fb30d4
rafaeldimaass <rafadimas29@gmail.com>
2026-05-27 10:24:34 +0700

    Modif Pertanyaan 7 Percobaan 1

--- Jobsheet10/Queue.java
+++ Jobsheet10/Queue.java
@@ -59,6 +59,7 @@ public class Queue {
     public void enqueue(int dt) {
         if (isFull()) {
             System.out.println("Queue sudah penuh");
+            System.exit(0);
         } else {
             if (isEmpty()) {
                 front = rear = 0;
@@ -77,6 +78,7 @@ public class Queue {
         int dt = 0;
         if (isEmpty()) {
             System.out.println("Queue masih kosong");
+            System.exit(0);
         } else {
             dt = data[front];
             size--;
