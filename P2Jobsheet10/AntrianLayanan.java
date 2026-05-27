a42393ca7c08768dec8227ddf29b0abb2105d696
rafaeldimaass <rafadimas29@gmail.com>
2026-05-27 11:05:38 +0700

    Percobaan 2 Queue

---
+++ P2Jobsheet10/AntrianLayanan.java
@@ -0,0 +1,90 @@
+package P2Jobsheet10;
+
+public class AntrianLayanan {
+    Mahasiswa[] data;
+    int front;
+    int rear;
+    int size;
+    int max;
+
+    public AntrianLayanan(int max) {
+        this.max = max;
+        this.data = new Mahasiswa[max];
+        this.front = 0;
+        this.rear = -1;
+        this.size = 0;
+    }
+    public boolean isEmpty() {
+        if (size == 0) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    public boolean isFull() {
+        if (size == max) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public void lihatTerdepan() {
+        if (isEmpty()) {
+            System.out.println("Antrian kosong");
+        }
+        else {
+            System.out.println("Mahasiswa terdepan: ");
+            System.out.println("NIM - NAMA - PRODI - KELAS");
+            data[front].tampilkanData();
+        }
+    }
+    public void tampilkanSemua() {
+        if(isEmpty()) {
+            System.out.println("Antrian masih kosong");
+            return;
+        }
+        System.out.println("Daftar mahasiswa dalam antrian: ");
+        System.out.println("NIM - NAMA - PRODI - KELAS");
+        for(int i = 0; i < size; i++) {
+            int index = (front +i) % max;
+            System.out.print((i+1) + ". ");
+            data[index].tampilkanData();
+        }
+        
+        
+    }
+    public void clear() {
+        if (!isEmpty()) {
+            front = rear = -1;
+            size = 0;
+            System.out.println("Queue berhasil dikosongkan");
+        } else {
+            System.out.println("Queue masih kosong");
+        }
+    }
+    public void tambahAntrian(Mahasiswa mhs) {
+        if (isFull()) {
+            System.out.println("Antrian sudah penuh, tidak dapat menambah mahasiswa");
+            return;
+        } 
+        rear = (rear + 1) % max;
+        data[rear] = mhs;
+        size++;
+        System.out.println(mhs.nama + " berhasil masuk ke antrian");
+    }
+    public Mahasiswa layaniMahasiswa() {
+        if (isEmpty()) {
+            System.out.println("Antrian masih kosong");
+            return null;
+        } 
+        Mahasiswa mhs = data[front];
+        front = (front + 1) % max;
+        size--;
+        return mhs;
+    }
+    public int getJumlahAntrian() {
+        return size;
+    }
+
+}
---
+++ P2Jobsheet10/LayananAkademikSIAKAD.java
@@ -0,0 +1,61 @@
+package P2Jobsheet10;
+
+import java.util.Scanner;
+
+public class LayananAkademikSIAKAD {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        AntrianLayanan antrian = new AntrianLayanan(5);
+        int pilihan;
+        do {
+            System.out.println("\n=== Menu Antrian Layanan Akademik ===");
+            System.out.println("1. Tambah Mahasiswa");
+            System.out.println("2. Layani Mahasiswa");
+            System.out.println("3. Lihat Mahasiswa Terdepan");
+            System.out.println("4. Lihat semua antrian");
+            System.out.println("5. Jumlah Mahasiswa dalam Antrian");
+            System.out.println("0. Keluar");
+            System.out.print("Pilih menu: ");
+            pilihan = input.nextInt();
+            input.nextLine();
+            switch (pilihan) {
+                case 1:
+                    System.out.println("NIM     : ");
+                    String nim = input.nextLine();
+                    System.out.println("Nama    : ");
+                    String nama = input.nextLine();
+                    System.out.println("Prodi   : ");
+                    String prodi = input.nextLine();
+                    System.out.println("Kelas   : ");
+                    String kelas = input.nextLine();
+                    Mahasiswa mhs =  new Mahasiswa(nim, nama, prodi, kelas);
+                    antrian.tambahAntrian(mhs);
+                    break;
+                case 2:
+                    Mahasiswa dilayani = antrian.layaniMahasiswa();
+                    if (dilayani != null) {
+                        System.out.print("Melayani mahasiswa: ");
+                        dilayani.tampilkanData();
+                    }
+                    break;
+                case 3:
+                    antrian.lihatTerdepan();
+                    break;
+                case 4:
+                    antrian.tampilkanSemua();
+                    break;
+                case 5:
+                    System.out.println("Jumlah dalam antrian: " + antrian.getJumlahAntrian());
+                    break;
+                case 0:
+                    System.out.println("Terima kasih");
+                    break;
+                default:
+                    System.out.println("Pilihan tidak valid");
+            }
+        } while (pilihan != 0);
+            
+        input.close();
+    }
+    
+}
